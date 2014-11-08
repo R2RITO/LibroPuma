88c2e659c2e48454b659e1e96bfa0ebb4520fa04
Macarena Santana <mpsanta1@uc.cl>
2014-11-08 11:06:54 -0300

    Marcador entre escenas

---
+++ config.lua
@@ -0,0 +1,31 @@
+application = {
+	content = {
+		width = 768,
+		height = 1024, 
+        --width = 0,
+        --height = 0,
+		scale = "letterBox",
+		fps = 30,
+		
+		--[[
+        imageSuffix = {
+		    ["@2x"] = 2,
+		}
+		--]]
+	},
+
+    --[[
+    -- Push notifications
+
+    notification =
+    {
+        iphone =
+        {
+            types =
+            {
+                "badge", "sound", "alert", "newsstand"
+            }
+        }
+    }
+    --]]    
+}
